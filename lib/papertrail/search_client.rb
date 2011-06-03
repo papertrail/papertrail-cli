9e8a0823906a79fb65692f631b2c837d0f861172
Eric Lindvall <eric@5stops.com>
2011-06-02 21:17:35 -0700

    Provide a mechanims to make the lack of a default openssl CApath on Ubuntu work.

--- lib/papertrail/search_client.rb
+++ lib/papertrail/search_client.rb
@@ -5,12 +5,22 @@ require 'always_verify_ssl_certificates'
 module Papertrail
   class SearchClient
     attr_accessor :username, :password, :conn
-  
+
     def initialize(username, password)
       @username = username
       @password = password
-    
-      @conn = Faraday::Connection.new(:url => 'https://papertrailapp.com', :ssl => { :verify => OpenSSL::SSL::VERIFY_PEER }) do |builder|
+
+      ssl_options = { :verify => OpenSSL::SSL::VERIFY_PEER }
+
+      # Make Ubuntu OpenSSL work
+      #
+      # From: https://bugs.launchpad.net/ubuntu/+source/openssl/+bug/396818
+      # "[OpenSSL] does not presume to select a set of CAs by default."
+      if File.directory?('/etc/ssl/certs/ca-certificates.crt')
+        ssl_options[:ca_file] = '/etc/ssl/certs/ca-certificates.crt'
+      end
+
+      @conn = Faraday::Connection.new(:url => 'https://papertrailapp.com', :ssl => ssl_options) do |builder|
         builder.basic_auth(@username, @password)
 
         builder.adapter  Faraday.default_adapter
@@ -19,7 +29,7 @@ module Papertrail
 
       @max_id_seen = {}
     end
-    
+
     # search for all events or a specific query, defaulting to all events since
     # last result set (call with since=0 for all).
     def search(q = nil, since = nil)
@@ -32,7 +42,7 @@ module Papertrail
         response.body['events']
       end
     end
-    
+
     def params_for_query(q = nil, since = nil)
       params = {}
       params[:q] = q if q
@@ -40,7 +50,7 @@ module Papertrail
       params[:min_id] = since if since
       params
     end
-    
+
     def self.format_events(events, &block)
       events.each do |event|
         yield "#{Time.parse(event['received_at']).strftime('%b %e %X')} #{event['hostname']} #{event['program']} #{event['message']}"
