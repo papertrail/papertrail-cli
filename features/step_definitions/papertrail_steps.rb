503ad6dcee12013a45e8d7e80a63e9c346bfdfe6
Cezary Baginski <cezary.baginski@gmail.com>
2012-02-04 02:11:58 +0100

    add test using system_id

--- features/step_definitions/papertrail_steps.rb
+++ features/step_definitions/papertrail_steps.rb
@@ -15,20 +15,31 @@ def stub_request(url, body)
   eoruby
 end
 
+Given /^the following systems:$/ do |systems|
+  url = "https://papertrailapp.com/api/v1/systems.json"
+  stub_request(url, systems.hashes.to_json)
+end
+
 Given /^the following server log:$/ do |logs|
   url = "https://papertrailapp.com/api/v1/events/search.json"
   stub_request(url, { :events => logs.hashes }.to_json)
+
+  url = "https://papertrailapp.com/api/v1/events/search.json?system_id"
+
+  #TODO: hardcoded (don't know how to extract the query)
+  events = logs.hashes.select {|ev| ev[:source_id] == '1234' }
+  stub_request(url, { :events => events }.to_json)
 end
 
-When /^I papertrail$/ do
+When /^I papertrail(?: with '([^']*)')?$/ do |params|
   stubbing_file = 'noweb.rb'
   write_file(stubbing_file, <<-ruby
     require 'webmock'
     include WebMock::API
-    #{@stubbed_requests.join('\n')}
+    #{@stubbed_requests.join("\n")}
     ruby
   )
 
-  cmd = "ruby -rbundler/setup -rwebmock -r./#{stubbing_file} ../../bin/papertrail"
+  cmd = "ruby -rbundler/setup -rwebmock -r./#{stubbing_file} -- ../../bin/papertrail #{params}"
   run_simple(cmd)
 end
