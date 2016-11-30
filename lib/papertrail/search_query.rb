68d250f9769ce31dbcb4df53594f4afa90b13501
sethherr <seth.william.herr@gmail.com>
2016-11-30 12:37:48 -0800

    Add initial search limit for first request

--- lib/papertrail/search_query.rb
+++ lib/papertrail/search_query.rb
@@ -6,15 +6,21 @@ module Papertrail
       '/api/v1/events/search.json'
     end
 
-    attr_reader :max_id
+    def self.initial_search_limit
+      100
+    end
+
+    attr_reader :max_id, :has_connected
 
     def search_results(connection, query = nil, options = {})
       params = options.dup
       params[:q] = query if query
       params[:min_id] = max_id if max_id
+      params[:limit] ||= self.class.initial_search_limit unless has_connected
 
       response = connection.get(self.class.api_url, params)
       @max_id = response.body['max_id']
+      @has_connected = true
       Papertrail::SearchResult.new(response.body)
     end
   end
--- test/search_query_test.rb
+++ test/search_query_test.rb
@@ -6,11 +6,19 @@ class SearchQueryTest < Minitest::Test
   let(:search_query) { Papertrail::SearchQuery.new }
   let(:get_response) { OpenStruct.new(:body => { 'max_id' => 122, 'events' => [] }) }
   let(:api_url) { Papertrail::SearchQuery.api_url }
+  let(:default_initial_params) { { limit: Papertrail::SearchQuery.initial_search_limit } }
 
   def test_sets_min_id_for_subsequent_requests_with_max_id
-    connection.expects(:get).with(api_url, {}).returns(get_response)
+    # Also testing that we set the initial_search_limit only for the first request
+    connection.expects(:get).with(api_url, default_initial_params).returns(get_response)
     search_query.search_results(connection)
     connection.expects(:get).with(api_url, { :min_id => 122 }).returns(get_response)
     search_query.search_results(connection)
+  end
+
+  def test_sets_limit_from_options
+    opts = { limit: 2 }
+    connection.expects(:get).with(api_url, opts).returns(get_response)
+    search_query.search_results(connection, nil, opts)
   end
 end
