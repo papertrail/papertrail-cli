7aab9c2fa8d116125e9c6b1acbbd3518990ff475
sethherr <seth.william.herr@gmail.com>
2016-12-02 11:08:43 -0800

    update travis.yml, https://github.com/travis-ci/travis-ci/issues/6963

--- test/connection_test.rb
+++ test/connection_test.rb
@@ -3,4 +3,8 @@ require 'test_helper'
 class ConnectionTest < Minitest::Test
   let(:connection_options) { { :token => 'dummy' } }
   let(:connection) { Papertrail::Connection.new(connection_options) }
+
+  def test_each_event
+    skip
+  end
 end
