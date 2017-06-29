a03ab6370b98247ef1f4c72497e17fe9ea549bfc
sethherr <seth.william.herr@gmail.com>
2017-06-29 11:51:19 -0700

    a bit of rubocop cleanup stuff

--- test/http_client_test.rb
+++ test/http_client_test.rb
@@ -3,22 +3,23 @@ require 'test_helper'
 class HttpClientTest < Minitest::Test
   describe "http methods" do
     let(:http) { Papertrail::HttpClient.new({}) }
+
     def test_cli_version_present_in_http_methods
-      skip if RUBY_VERSION < '1.9'
+      skip if RUBY_VERSION < '2.0'
       # if webmock.disable_net_connect intervenes with a stack trace,
       # we know the http methods are not sending cli_version in params
-      stub_request(:get, /https:\/\/papertrailapp.com\/api\/v1\/some-path\?cli_version=#{Papertrail::VERSION}/).to_return(:status => 200, :body => "", :headers => {})
-      http.get("some-path")
+      stub_request(:get, "https://papertrailapp.com/api/v1/some-path?cli_version=#{Papertrail::VERSION}").to_return(:status => 200, :body => '', :headers => {})
+      http.get('some-path')
 
-      stub_request(:post, "https://papertrailapp.com/api/v1/some-path").with(:body => "cli_version=#{Papertrail::VERSION}").to_return(:status => 200, :body => "", :headers => {})
-      http.post("some-path", {})
+      stub_request(:post, 'https://papertrailapp.com/api/v1/some-path').with(:body => "cli_version=#{Papertrail::VERSION}").to_return(:status => 200, :body => '', :headers => {})
+      http.post('some-path', {})
 
 
-      stub_request(:put, "https://papertrailapp.com/api/v1/some-path").with(:body => "cli_version=#{Papertrail::VERSION}").to_return(:status => 200, :body => "", :headers => {})
-      http.put("some-path", {})
+      stub_request(:put, 'https://papertrailapp.com/api/v1/some-path').with(:body => "cli_version=#{Papertrail::VERSION}").to_return(:status => 200, :body => '', :headers => {})
+      http.put('some-path', {})
 
-      stub_request(:delete, /https:\/\/papertrailapp.com\/api\/v1\/some-path\?cli_version=#{Papertrail::VERSION}/).to_return(:status => 200, :body => "", :headers => {})
-      http.delete("some-path")
+      stub_request(:delete, "https://papertrailapp.com/api/v1/some-path?cli_version=#{Papertrail::VERSION}").to_return(:status => 200, :body => '', :headers => {})
+      http.delete('some-path')
     end
   end
 end
